394e914d886eb4633602122cf9f515480cb806a4
iris-mcdonald <32845453+iris-mcdonald@users.noreply.github.com>
2018-02-01 08:09:03 -0500

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -1,77 +1,68 @@
-
-startProcess()
-{
-    for (var box = 1; box <= 3; box++)//process each box
-    {
-        if (box == 1) {
-            boxClass = "one";
-        } else if (box == 2) {
-            boxClass = "two";
-        } else if (box == 3) {
-            boxClass = "three";
-        } else {
-            boxClass = "unknown";
-        }
-
-        for (var i = 1; i >= 10; i++)//run change box color function 10X
+        function chgBoxColor()
         {
-            chgBoxColor();
-        }//end of for loop for change box color 
-
-        //set msg. depending on colors
-        If ((document.getElementById("one").className == document.getElementById("two").className) &&
-            (document.getElementById("two").className == document.getElementById("three").className)){
-            document.getElementById("msg").innerHTML = "Congratulations, you won!";
-        } else {
-            document.getElementById("msg").innerHTML = "Sorry, try again!"
-        }
-           
-
-    }//end of for loop for box
-
-}//end start process
-
-      
-/*run random number generator function until you get a number 1,2, or 3*/
-     function chgBoxColor()
-     { 
-            var result = 4;
-            if (box == 1) {
-
-            } 
-            while ((result == 0) || (result > 3))
-            {
+            var result = 0;
+            while ((result == 0) || (result > 3)) {//run for each box
                 var result = myRandomNumber();
             }
-
+            
             switch (result)
             {
                 case 1:
-                    document.write('One: ' + result);//add set class color to red
-                    var el = document.getElementById('boxId');
-                    el.className = "stop";
+                    /*document.write('Case One: ' + result);*/
+                    document.getElementById(boxId).className = "stop";//set box color to red
                     break;
                 case 2:
-                    document.write('Two: ' + result);//add set class color to yellow
-                    var el = document.getElementById('boxId');
-                    el.classname = "caution";
+                    /*document.write('Case Two: ' + result);*/
+                    document.getElementById(boxId).className = "caution";//set box color to yellow
                     break;
                 case 3:
-                    document.write('Three: ' + result);//add set class color to green
-                    var el = document.getElementById('boxId');
-                    el.classname = "green"
+                    /*document.write('Case Three: ' + result);//set box color to green*/
+                    document.getElementById(boxId).className = "go";
                     break;
                 default:
-                    document.write('Not One, Two, or Three ' + result);//need any extra code here?
+                    document.write('Case Not One, Two, or Three ' + result);//no color set
                     break;
             }
-     }
+        }
 
-function myRandomNumber()
-{
-    var result = Math.floor((Math.random() * 10) + 1);
-    document.write("random generator" + result);
-    return result;
-}
+        function myRandomNumber()
+        {
+            var result = Math.floor((Math.random() * 10) + 1);
+            /*document.write("random generator: " + result);*/
+            return result;
+        }
 
+        function startProcess()
+        {
+            for (var box = 1; box <= 3; box++)//process the three text boxes
+            {
+                if (box == 1) {
+                    boxId = "one";
+                } else if (box == 2) {
+                    boxId = "two";
+                } else {
+                    boxId = "three";
+                }
+                
+                for ( var i = 1; i <= 10; i++)//run change box color function 10X for each text box
+                {
+                    chgBoxColor();
+                } 
+            }//end of process text box loop
 
+            var box1Color = document.getElementById("one").className;
+            var box2Color = document.getElementById("two").className;
+            var box3Color = document.getElementById("three").className;
+            if ((box1Color == box2Color) && (box2Color == box3Color)) {//Set text msg based on box color
+                 document.getElementById("msg").innerHTML = "Congratulations, you won!";
+            } else {
+                 document.getElementById("msg").innerHTML = "Sorry, try again!";
+            }
+          
+        }//end of startProcess
+
+        startProcess();
+
+     
+
+   
